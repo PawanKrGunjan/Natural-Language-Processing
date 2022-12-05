25888bb67fccf85c4e27bba0bd765b6e0ab4488f
Pawan Kumar Gunjan <PawanKumarGunjan@hotmail.com>
2022-12-05 13:43:00 +0530

    Regular Expressions

---
+++ Learn/Regular Expressions, Text Normalization, Edit Distance/Minimum Edit Distance/Minimum Edit Distance.py
@@ -0,0 +1,52 @@
+def iterative_levenshtein(s, t, costs=(1, 1, 1)):
+    """ 
+        iterative_levenshtein(s, t) -> ldist
+        ldist is the Levenshtein distance between the strings 
+        s and t.
+        For all i and j, dist[i,j] will contain the Levenshtein 
+        distance between the first i characters of s and the 
+        first j characters of t
+        
+        costs: a tuple or a list with three integers (d, i, s)
+               where d defines the costs for a deletion
+                     i defines the costs for an insertion and
+                     s defines the costs for a substitution
+    """
+
+    rows = len(s)+1
+    cols = len(t)+1
+    deletes, inserts, substitutes = costs
+    
+    dist = [[0 for x in range(cols)] for x in range(rows)]
+
+    # source prefixes can be transformed into empty strings 
+    # by deletions:
+    for row in range(1, rows):
+        dist[row][0] = row * deletes
+
+    # target prefixes can be created from an empty source string
+    # by inserting the characters
+    for col in range(1, cols):
+        dist[0][col] = col * inserts
+        
+    for col in range(1, cols):
+        for row in range(1, rows):
+            if s[row-1] == t[col-1]:
+                cost = 0
+            else:
+                cost = substitutes
+            dist[row][col] = min(dist[row-1][col] + deletes,
+                                 dist[row][col-1] + inserts,
+                                 dist[row-1][col-1] + cost) # substitution
+
+    for r in range(rows):
+        print(dist[r])
+    
+ 
+    return dist[row][col]
+
+# default:
+print(iterative_levenshtein("INTENTION", "EXECUTION"))
+print()
+# the costs for substitions are twice as high as inserts and delets:
+print(iterative_levenshtein("INTENTION", "EXECUTION", costs=(1, 1, 2)))
